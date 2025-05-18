48b364dd1c83c474cfbd8cfbffd037ceeed53450
SatyanathBhatIIT <satyanath@iitgoa.ac.in>
2025-05-19 00:40:35 +0530

    more features

--- FileCompare.py
+++ FileCompare.py
@@ -61,7 +61,78 @@ def isInList(coap, listOfIssues):
             reason = listOfIssues[coap]
         
     return foundInList, reason
-        
+
+def compareFiles(df1, df2):
+    primaryKeyFile1 = 'App no'
+    primaryKeyFile2 = 'AppNo'
+    #compare these cols -  Email	Contact Number	Full Name	Adm cat	Pwd	Ews	Gender	Category	COAP	GATE25RollNo	GATE25Rank	GATE25Score	GATE25Disc	GATE24RollNo	GATE24Rank	GATE24Score	GATE24Disc	GATE23RollNo	GATE23Rank	GATE23Score	GATE23Disc	MaxGATEScore out of 3 yrs	HSSC(board)	HSSC(date)	HSSC(per)	SSC(board)	SSC(date)	SSC(per)	Degree(Qualification)	Degree(PassingDate)	Degree(Branch)	Degree(OtherBranch)	Degree(Institute Name)	Degree(CGPA-7thSem)	Degree(CGPA-8thSem)	Degree(Per-7thSem)
+    #colsToCompare = ["Email","Contact Number","Full Name",	"Adm cat", "Pwd","Ews","Gender","Category","COAP","GATE25RollNo", "GATE25Rank","GATE25Score","GATE25Disc","GATE24RollNo","GATE24Rank","GATE24Score","GATE24Disc","GATE23RollNo","GATE23Rank","GATE23Score","GATE23Disc","MaxGATEScore out of 3 yrs","HSSC(board)","HSSC(per)","SSC(board)","SSC(date)",	"SSC(per)"	,"Degree(Qualification)"	,"Degree(PassingDate)",	"Degree(Branch)"	,"Degree(OtherBranch)"	,"Degree(Institute Name)","Degree(CGPA-7thSem)"	,"Degree(CGPA-8thSem)",	"Degree(Per-7thSem)"]
+    #without ['SSC(date)', 'Degree(PassingDate)']
+    colsToCompare = ["Email","Contact Number","Full Name",	"Adm cat", "Pwd","Ews","Gender","Category","COAP","GATE25RollNo", "GATE25Rank","GATE25Score","GATE25Disc","GATE24RollNo","GATE24Rank","GATE24Score","GATE24Disc","GATE23RollNo","GATE23Rank","GATE23Score","GATE23Disc","MaxGATEScore out of 3 yrs","HSSC(board)","HSSC(per)","SSC(board)",	"SSC(per)"	,"Degree(Qualification)",	"Degree(Branch)"	,"Degree(OtherBranch)"	,"Degree(Institute Name)","Degree(CGPA-7thSem)"	,"Degree(CGPA-8thSem)",	"Degree(Per-7thSem)"]
+    #Without GATE25Rank 
+    colsToCompare = ["Email","Contact Number","Full Name",	"Adm cat", "Pwd","Ews","Gender","Category","COAP","GATE25RollNo", "GATE25Score","GATE25Disc","GATE24RollNo","GATE24Score","GATE24Disc","GATE23RollNo","GATE23Score","GATE23Disc","MaxGATEScore out of 3 yrs","HSSC(board)","HSSC(per)","SSC(board)",	"SSC(per)"	,"Degree(Qualification)",	"Degree(Branch)"	,"Degree(OtherBranch)"	,"Degree(Institute Name)","Degree(CGPA-7thSem)"	,	"Degree(Per-7thSem)"]
+
+    #create a dictionary of colname mapping: default mapping is same
+    colNameMapping = {}
+    for col in colsToCompare:
+        colNameMapping[col] = col
+    colNameMapping['Full Name'] = 'FullName'
+    colNameMapping['GATE25RollNo'] = 'currYearRollNo'
+    #colNameMapping['GATE25Rank'] = 'currYearRank'
+    colNameMapping['GATE25Score'] = 'currYearScore'
+    #colNameMapping['GATE25Disc'] = 'currYearDisc'
+    colNameMapping['GATE24RollNo'] = 'prevYearRollNo'
+    #colNameMapping['GATE24Rank'] = 'prevYearRank'
+    colNameMapping['GATE24Score'] = 'prevYearScore'
+    #colNameMapping['GATE24Disc'] = 'prevYearDisc'
+    colNameMapping['GATE23RollNo'] = 'prevprevYearRollNo'
+    #colNameMapping['GATE23Rank'] = 'prevprevYearRank'
+    colNameMapping['GATE23Score'] = 'prevprevYearScore'
+    #colNameMapping['GATE23Disc'] = 'prevprevYearDisc'
+    colNameMapping['MaxGATEScore out of 3 yrs'] = 'MaxGateScore'
+    colNameMapping['HSSC(per)']= 'HSSCper'
+    colNameMapping['SSC(per)']= 'SSCper'
+    colNameMapping['Degree(CGPA-8thSem)']= 'DegreeCGPA8thSem'
+    colNameMapping['Degree(Per-8thSem)']= 'DegreePer8thSem'
+
+
+
+
+
+
+    presentOnlyInFile1 = {}
+    rowsDifferentFields = {}
+
+    #start compare
+    for index, row in df1.iterrows():
+        coap = row[primaryKeyFile1]
+        if coap not in df2[primaryKeyFile2].values:
+            presentOnlyInFile1[coap] = True
+        else:
+            row2 = df2.loc[df2[primaryKeyFile2] == coap]
+            for col in colsToCompare:
+                if col =='COAP':
+                    #check if there is a -1 or -2 or -3 at end for the value
+                    if len(row[col])>2:
+                        if row[col][-2]=='-':
+                            print("COAP value has -1 or -2 or -3 at end:" , row[col])
+                        #similar check for row2
+                        if row2[col].values[0][-2]=='-':
+                            print("COAP value has -1 or -2 or -3 at end", row2[col].values[0])
+                    else:
+                        print("Strange Len", row[col],coap)
+                                        
+                col2 = colNameMapping[col]
+                if row[col] != row2[col2].values[0]:
+                    if coap not in rowsDifferentFields:
+                        rowsDifferentFields[coap] = []                
+                    rowsDifferentFields[coap].append(col)
+                    
+    #end compare
+    #print("Present only in file 1: ", presentOnlyInFile1.keys())
+    print("total present only in file 1: ", len(presentOnlyInFile1))
+
+    print("Rows with different fields: ", rowsDifferentFields)
 
 def getDecision(coap, clarityList, rejectList):
     decision = 'Y'
@@ -76,77 +147,36 @@ def getDecision(coap, clarityList, rejectList):
             decision = '?'
     return decision, reason
 
-file1 = '/home/satyanath/data/MTech2025/CSE_raw_Mtech_2025_manual_corrections (1).xlsx'
-file2 = '/home/satyanath/data/MTech2025/round0_CSE_File.xlsx'
-extraRow1 = True
-extraRow2 = False
+fileList1 = [
+    ('/home/satyanath/data/MTech2025/CSE_raw_Mtech_2025_manual_corrections (1).xlsx',True),
+    ('/home/satyanath/data/MTech2025/MasterData_EC.xlsx',False),
+    ('/home/satyanath/data/MTech2025/MasterData_EE.xlsx',False),
+    ('/home/satyanath/data/MTech2025/CHE_Master_Data.xlsx',False),
+    ('/home/satyanath/data/MTech2025/ME.xlsx',True),
+    
+]
+fileList2 = [
+    ('/home/satyanath/data/MTech2025/round0_CSE_File.xlsx',False),
+    ('/home/satyanath/data/MTech2025/round0_EC_file.xlsx',False),
+    ('/home/satyanath/data/MTech2025/round0_EE_File.xlsx',False),
+    ('/home/satyanath/data/MTech2025/round0_CHE_File.xlsx',False),
+    ('/home/satyanath/data/MTech2025/round0_ME_File.xlsx',False),
+]
 
-df1 = createDataframeFromExcel(file1, extraRow1)
-df2 = createDataframeFromExcel(file2, extraRow2)
+#fileList1 and fileList2 are same size and corresponding files need to be compared
+for idx,elem in enumerate(fileList1):
+    file1, extraRow1 = fileList1[idx]
+    file2, extraRow2 = fileList2[idx]  
+    
 
-print(df1.columns)
-print(df2.columns)
-
-
-
-primaryKeyFile1 = 'App no'
-primaryKeyFile2 = 'AppNo'
-#compare these cols -  Email	Contact Number	Full Name	Adm cat	Pwd	Ews	Gender	Category	COAP	GATE25RollNo	GATE25Rank	GATE25Score	GATE25Disc	GATE24RollNo	GATE24Rank	GATE24Score	GATE24Disc	GATE23RollNo	GATE23Rank	GATE23Score	GATE23Disc	MaxGATEScore out of 3 yrs	HSSC(board)	HSSC(date)	HSSC(per)	SSC(board)	SSC(date)	SSC(per)	Degree(Qualification)	Degree(PassingDate)	Degree(Branch)	Degree(OtherBranch)	Degree(Institute Name)	Degree(CGPA-7thSem)	Degree(CGPA-8thSem)	Degree(Per-7thSem)
-#colsToCompare = ["Email","Contact Number","Full Name",	"Adm cat", "Pwd","Ews","Gender","Category","COAP","GATE25RollNo", "GATE25Rank","GATE25Score","GATE25Disc","GATE24RollNo","GATE24Rank","GATE24Score","GATE24Disc","GATE23RollNo","GATE23Rank","GATE23Score","GATE23Disc","MaxGATEScore out of 3 yrs","HSSC(board)","HSSC(per)","SSC(board)","SSC(date)",	"SSC(per)"	,"Degree(Qualification)"	,"Degree(PassingDate)",	"Degree(Branch)"	,"Degree(OtherBranch)"	,"Degree(Institute Name)","Degree(CGPA-7thSem)"	,"Degree(CGPA-8thSem)",	"Degree(Per-7thSem)"]
-#without ['SSC(date)', 'Degree(PassingDate)']
-colsToCompare = ["Email","Contact Number","Full Name",	"Adm cat", "Pwd","Ews","Gender","Category","COAP","GATE25RollNo", "GATE25Rank","GATE25Score","GATE25Disc","GATE24RollNo","GATE24Rank","GATE24Score","GATE24Disc","GATE23RollNo","GATE23Rank","GATE23Score","GATE23Disc","MaxGATEScore out of 3 yrs","HSSC(board)","HSSC(per)","SSC(board)",	"SSC(per)"	,"Degree(Qualification)",	"Degree(Branch)"	,"Degree(OtherBranch)"	,"Degree(Institute Name)","Degree(CGPA-7thSem)"	,"Degree(CGPA-8thSem)",	"Degree(Per-7thSem)"]
-
-
-#create a dictionary of colname mapping: default mapping is same
-colNameMapping = {}
-for col in colsToCompare:
-    colNameMapping[col] = col
-colNameMapping['Full Name'] = 'FullName'
-colNameMapping['GATE25RollNo'] = 'currYearRollNo'
-#colNameMapping['GATE25Rank'] = 'currYearRank'
-colNameMapping['GATE25Score'] = 'currYearScore'
-#colNameMapping['GATE25Disc'] = 'currYearDisc'
-colNameMapping['GATE24RollNo'] = 'prevYearRollNo'
-#colNameMapping['GATE24Rank'] = 'prevYearRank'
-colNameMapping['GATE24Score'] = 'prevYearScore'
-#colNameMapping['GATE24Disc'] = 'prevYearDisc'
-colNameMapping['GATE23RollNo'] = 'prevprevYearRollNo'
-#colNameMapping['GATE23Rank'] = 'prevprevYearRank'
-colNameMapping['GATE23Score'] = 'prevprevYearScore'
-#colNameMapping['GATE23Disc'] = 'prevprevYearDisc'
-colNameMapping['MaxGATEScore out of 3 yrs'] = 'MaxGateScore'
-colNameMapping['HSSC(per)']= 'HSSCper'
-colNameMapping['SSC(per)']= 'SSCper'
-colNameMapping['Degree(CGPA-8thSem)']= 'DegreeCGPA8thSem'
-colNameMapping['Degree(Per-8thSem)']= 'DegreePer8thSem'
+    df1 = createDataframeFromExcel(file1, extraRow1)
+    df2 = createDataframeFromExcel(file2, extraRow2)
+    
+    compareFiles(df1, df2)
+    print("********")
 
 
 
-
-
-
-presentOnlyInFile1 = {}
-rowsDifferentFields = {}
-
-#start compare
-for index, row in df1.iterrows():
-    coap = row[primaryKeyFile1]
-    if coap not in df2[primaryKeyFile2].values:
-        presentOnlyInFile1[coap] = True
-    else:
-        row2 = df2.loc[df2[primaryKeyFile2] == coap]
-        for col in colsToCompare:
-            col2 = colNameMapping[col]
-            if row[col] != row2[col2].values[0]:
-                if coap not in rowsDifferentFields:
-                    rowsDifferentFields[coap] = []                
-                rowsDifferentFields[coap].append(col)
-                
-#end compare
-#print("Present only in file 1: ", presentOnlyInFile1.keys())
-print("total present only in file 1: ", len(presentOnlyInFile1))
-
-print("Rows with different fields: ", rowsDifferentFields)
 
 
 
